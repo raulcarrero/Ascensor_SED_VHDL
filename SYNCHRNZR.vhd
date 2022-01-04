3ddc68d0fbc3df9ace41ebb40a161c38f6aaf4f6
Jimmy21s <97113180+Jimmy21s@users.noreply.github.com>
2022-01-04 20:33:12 +0100

    Update SYNCHRNZR.vhd

--- SYNCHRNZR.vhd
+++ SYNCHRNZR.vhd
@@ -31,9 +31,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SYNCHRNZR is
     port (
-        CLK      : in  std_logic;
-        ASYNC_IN : in  std_logic;
-        SYNC_OUT : out std_logic
+        CLK      : in  std_logic;   --Reloj común
+        ASYNC_IN : in  std_logic;   --Entrada asíncrona
+        SYNC_OUT : out std_logic    --Salida síncrona
     );
 end SYNCHRNZR;
 
